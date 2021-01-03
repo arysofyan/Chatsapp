e13c111941307324c2824acfe41187d89bc77e99
arysofyan <76903161+arysofyan@users.noreply.github.com>
2021-01-03 18:10:07 +0700

    Initial commit

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA7c5v06eqnzX_kUdZdJ9jsAebEHoYF-a0",
+  authDomain: "chat-app-ary.firebaseapp.com",
+  databaseURL: "https://chat-app-ary.firebaseio.com",
+  projectId: "chat-app-ary",
+  storageBucket: "chat-app-ary.appspot.com",
+  messagingSenderId: "1040085348257",
+  appId: "1:1040085348257:web:5275a0b497963e7718a5e0"
+};
+
+export default firebaseConfig;
